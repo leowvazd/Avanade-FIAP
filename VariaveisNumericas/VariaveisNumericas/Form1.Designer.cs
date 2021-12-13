c4537ee5044d8241c48b3829e5bd87fcbad56499
leowvazd <leonardovazlourenco@gmail.com>
2021-12-13 18:33:46 -0300

    up
    up

---
+++ VariaveisNumericas/VariaveisNumericas/Form1.Designer.cs
@@ -0,0 +1,119 @@
+﻿
+namespace VariaveisNumericas
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtNum1 = new System.Windows.Forms.TextBox();
+            this.txtNum2 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btnSomar = new System.Windows.Forms.Button();
+            this.lblRes = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 10);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(53, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Número 1";
+            // 
+            // txtNum1
+            // 
+            this.txtNum1.Location = new System.Drawing.Point(15, 39);
+            this.txtNum1.Name = "txtNum1";
+            this.txtNum1.Size = new System.Drawing.Size(100, 20);
+            this.txtNum1.TabIndex = 1;
+            // 
+            // txtNum2
+            // 
+            this.txtNum2.Location = new System.Drawing.Point(142, 39);
+            this.txtNum2.Name = "txtNum2";
+            this.txtNum2.Size = new System.Drawing.Size(100, 20);
+            this.txtNum2.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(140, 10);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(53, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Número 2";
+            // 
+            // btnSomar
+            // 
+            this.btnSomar.Location = new System.Drawing.Point(15, 75);
+            this.btnSomar.Name = "btnSomar";
+            this.btnSomar.Size = new System.Drawing.Size(227, 23);
+            this.btnSomar.TabIndex = 4;
+            this.btnSomar.Text = "Somar";
+            this.btnSomar.UseVisualStyleBackColor = true;
+            this.btnSomar.Click += new System.EventHandler(this.btnSomar_Click);
+            // 
+            // lblRes
+            // 
+            this.lblRes.AutoSize = true;
+            this.lblRes.ForeColor = System.Drawing.Color.Red;
+            this.lblRes.Location = new System.Drawing.Point(12, 112);
+            this.lblRes.Name = "lblRes";
+            this.lblRes.Size = new System.Drawing.Size(0, 13);
+            this.lblRes.TabIndex = 5;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(257, 144);
+            this.Controls.Add(this.lblRes);
+            this.Controls.Add(this.btnSomar);
+            this.Controls.Add(this.txtNum2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtNum1);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Somar";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtNum1;
+        private System.Windows.Forms.TextBox txtNum2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btnSomar;
+        private System.Windows.Forms.Label lblRes;
+    }
+}
+
---
+++ VariaveisNumericas/VariaveisNumericas/Form1.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace VariaveisNumericas
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnSomar_Click(object sender, EventArgs e)
+        {
+            // DECLARANDO AS VARIAVEIS
+
+            double numero1, numero2, resultado;                 // até 8 decimais
+
+            // LENDO OS VALORES
+
+            numero1 = Convert.ToDouble(txtNum1.Text);
+            numero2 = Convert.ToDouble(txtNum2.Text);
+
+            // AQUI CALCULAMOS O RESULTADO DA SOMA
+
+            resultado = numero1 + numero2;
+
+            // MOSTRANDO O RESULTADO NA LABEL3
+
+            lblRes.Text = resultado.ToString("0.00");
+
+        }
+    }
+}
