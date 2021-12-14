a8d93023e721d34162f1523d32a6dd40f393e079
leowvazd <leonardovazlourenco@gmail.com>
2021-12-14 15:13:47 -0300

    up
    up

---
+++ IfSimples/IfSimples/Form1.Designer.cs
@@ -0,0 +1,89 @@
+﻿
+namespace IfSimples
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtIdade = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.bntVerificar = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txtIdade
+            // 
+            this.txtIdade.Location = new System.Drawing.Point(153, 33);
+            this.txtIdade.Name = "txtIdade";
+            this.txtIdade.Size = new System.Drawing.Size(100, 20);
+            this.txtIdade.TabIndex = 0;
+            this.txtIdade.TextChanged += new System.EventHandler(this.txtIdade_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(57, 36);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(90, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Idade do Jogador";
+            // 
+            // bntVerificar
+            // 
+            this.bntVerificar.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.bntVerificar.Location = new System.Drawing.Point(60, 72);
+            this.bntVerificar.Name = "bntVerificar";
+            this.bntVerificar.Size = new System.Drawing.Size(193, 23);
+            this.bntVerificar.TabIndex = 2;
+            this.bntVerificar.Text = "Verificar";
+            this.bntVerificar.UseVisualStyleBackColor = true;
+            this.bntVerificar.Click += new System.EventHandler(this.bntVerificar_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.MenuHighlight;
+            this.ClientSize = new System.Drawing.Size(308, 124);
+            this.Controls.Add(this.bntVerificar);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtIdade);
+            this.Name = "Form1";
+            this.Text = "Jogo - Drogas";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtIdade;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button bntVerificar;
+    }
+}
+
---
+++ IfSimples/IfSimples/Form1.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace IfSimples
+{
+    public partial class Form1 : Form
+    {
+        byte idade;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void txtIdade_TextChanged(object sender, EventArgs e)
+        {
+            idade = Convert.ToByte(txtIdade.Text);
+        }
+
+        private void bntVerificar_Click(object sender, EventArgs e)
+        {
+            if (idade >= 12)
+            {
+                MessageBox.Show("Você pode jogar! :)", "Mensagem!", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+
+            else
+            {
+                MessageBox.Show("Você não pode jogar! :(", "Mensagem!", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+        }
+    }
+}
