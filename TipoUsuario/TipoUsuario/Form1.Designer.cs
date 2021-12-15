8db5b8d0d6eb8e19488a34b2f6e6a82177ec99b8
leowvazd <leonardovazlourenco@gmail.com>
2021-12-15 13:14:52 -0300

    up
    up

---
+++ TipoUsuario/TipoUsuario/Form1.Designer.cs
@@ -0,0 +1,87 @@
+﻿
+namespace TipoUsuario
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtUsuario = new System.Windows.Forms.TextBox();
+            this.Verificar = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(88, 50);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(124, 20);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Tipo de Usuário:";
+            // 
+            // txtUsuario
+            // 
+            this.txtUsuario.Location = new System.Drawing.Point(218, 47);
+            this.txtUsuario.Name = "txtUsuario";
+            this.txtUsuario.Size = new System.Drawing.Size(177, 26);
+            this.txtUsuario.TabIndex = 1;
+            // 
+            // Verificar
+            // 
+            this.Verificar.Location = new System.Drawing.Point(92, 153);
+            this.Verificar.Name = "Verificar";
+            this.Verificar.Size = new System.Drawing.Size(303, 44);
+            this.Verificar.TabIndex = 2;
+            this.Verificar.Text = "Verificar";
+            this.Verificar.UseVisualStyleBackColor = true;
+            this.Verificar.Click += new System.EventHandler(this.Verificar_Click);
+            // 
+            // Form1
+            // 
+            this.AcceptButton = this.Verificar;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveBorder;
+            this.ClientSize = new System.Drawing.Size(516, 265);
+            this.Controls.Add(this.Verificar);
+            this.Controls.Add(this.txtUsuario);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtUsuario;
+        private System.Windows.Forms.Button Verificar;
+    }
+}
+
---
+++ TipoUsuario/TipoUsuario/Form1.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TipoUsuario
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Verificar_Click(object sender, EventArgs e)
+        {
+            string usuario;
+
+            usuario = txtUsuario.Text.ToUpper();
+
+            if (usuario != "ADMINISTRADOR")
+            {
+                MessageBox.Show("Você não é administrador! :(", "Login!", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+
+            else
+            {
+                MessageBox.Show("Você é administrador! :)", "Login!", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+        }
+    }
+}
